0937b3c86e19212652a94ce8fe6a0a5dcef78316
y_dove <1352035939@qq.com>
2023-02-12 21:37:14 +0800

    lab5

---
+++ lab5/server/api.js
@@ -0,0 +1,78 @@
+/*
+|--------------------------------------------------------------------------
+| api.js -- server routes
+|--------------------------------------------------------------------------
+|
+| This file defines the routes for your server.
+|
+*/
+
+const express = require("express");
+
+const router = express.Router();
+
+// Use a hardcoded username for now
+const myName = "Anonymous";
+
+const data = {
+  stories: [
+    {
+      _id: 0,
+      creator_name: "Nicholas Tsao",
+      content: "I love dancing!",
+    },
+  ],
+  comments: [
+    {
+      _id: 0,
+      creator_name: "Philena Liu",
+      parent: 0,
+      content: "Me Too!",
+    },
+  ],
+};
+
+router.get("/test", (req, res) => {
+  res.send({ message: "Wow I made my first API! In its own file!" });
+});
+
+router.get("/stories", (req, res) => {
+  // send back all of the stories!
+  res.send(data.stories);
+});
+
+router.get("/comment", (req, res) => {
+  const filteredComments = data.comments.filter((comment) => comment.parent == req.query.parent);
+  res.send(filteredComments);
+});
+
+router.post("/story", (req, res) => {
+  const newStory = {
+    _id: data.stories.length,
+    creator_name: myName,
+    content: req.body.content,
+  };
+
+  data.stories.push(newStory);
+  res.send(newStory);
+});
+
+router.post("/comment", (req, res) => {
+  const newComment = {
+    _id: data.comments.length,
+    creator_name: myName,
+    parent: req.body.parent,
+    content: req.body.content,
+  };
+
+  data.comments.push(newComment);
+  res.send(newComment);
+});
+
+// catch all /api route
+router.all("*", (req, res) => {
+  console.log(`API Route not found: ${req.method} ${req.url}`);
+  res.status(404).send({ message: "API Route not found" });
+});
+
+module.exports = router;
